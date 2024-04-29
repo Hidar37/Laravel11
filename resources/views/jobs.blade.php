3b5aa822809317d8d6213e4e81a85d9b1fae346d
Haider Ahmad <hidarahmadi5@gmail.com>
2024-04-29 22:51:57 +0430

    View method
    - I have learn how to send wildcard (send data using URL).
    - I have learn how to send data using view() method to view page and how to access it.

---
+++ resources/views/jobs.blade.php
@@ -0,0 +1,13 @@
+<x-layout>
+    <x-slot:heading>
+        Jobs Listing
+    </x-slot:heading>
+    <ul>
+        @foreach ($jobs as $job)
+        <li>
+            <a href="/job/{{ $job['id'] }}" class="text-blue-500 hover:underline"><strong>{{ $job['title'] }}</strong>: Pays {{ $job['salary'] }} per year.</a>
+        </li>
+        @endforeach
+    </ul>
+
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,54 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/jobs', function () {
+    return view('jobs', [
+        'jobs' => [
+            [
+                'id' => 1,
+                'title' => 'PHP Developer',
+                'salary' => '$50.000'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Programmer',
+                'salary' => '$60.000',
+            ],
+            [
+                'id' => 3,
+                'title' => 'Web Developer',
+                'salary' => '$70.000'
+            ]
+        ]
+    ]);
+});
+
+Route::get('/job/{id}', function ($id) {
+    $jobs = [
+        [
+            'id' => 1,
+            'title' => 'PHP Developer',
+            'salary' => '$50.000'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Programmer',
+            'salary' => '$60.000',
+        ],
+        [
+            'id' => 3,
+            'title' => 'Web Developer',
+            'salary' => '$70.000'
+        ]
+    ];
+    $job = Arr::first($jobs, fn ($job) => $job['id'] == $id);
+    return view('job', ['job' => $job]);
 });
 
 Route::get('/contact', function () {
