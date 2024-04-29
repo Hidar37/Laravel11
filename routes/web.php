f461a78232e6e46806aad14dac7329617a9e6b41
Haider Ahmad <hidarahmadi5@gmail.com>
2024-04-29 23:04:18 +0430

    Job Not found
    - I have cover the exception of if the job is not found, It is now going to redirect the user back to the job listing page.

--- routes/web.php
+++ routes/web.php
@@ -48,7 +48,31 @@ Route::get('/job/{id}', function ($id) {
         ]
     ];
     $job = Arr::first($jobs, fn ($job) => $job['id'] == $id);
-    return view('job', ['job' => $job]);
+    // If Job is found in the array display the Job
+    // Else Redirect the user back to the Job Listing page.
+    if ($job) {
+        return view('job', ['job' => $job]);
+    } else {
+        return view('jobs', [
+            'jobs' => [
+                [
+                    'id' => 1,
+                    'title' => 'PHP Developer',
+                    'salary' => '$50.000'
+                ],
+                [
+                    'id' => 2,
+                    'title' => 'Programmer',
+                    'salary' => '$60.000',
+                ],
+                [
+                    'id' => 3,
+                    'title' => 'Web Developer',
+                    'salary' => '$70.000'
+                ]
+            ]
+        ]);
+    }
 });
 
 Route::get('/contact', function () {
